3ee299978021a6598a68abc368fe78d6717a2dbb
siekiera84 <kowalczyk.hubert.t@gmail.com>
2022-04-25 21:25:55 +0200

    final!

---
+++ precache-manifest.bcaac0898c5ac22bed5e897a43b0661f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "224c44400ae77ded3fa0",
+    "url": "/static/css/main.a685fd52.chunk.css"
+  },
+  {
+    "revision": "224c44400ae77ded3fa0",
+    "url": "/static/js/main.5f6fd3af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eb18e55880dcc0d957fc",
+    "url": "/static/css/2.fd094d6c.chunk.css"
+  },
+  {
+    "revision": "eb18e55880dcc0d957fc",
+    "url": "/static/js/2.cf0653fb.chunk.js"
+  },
+  {
+    "revision": "033612c825a945442a768ef2247067d9",
+    "url": "/static/media/pokepedia.033612c8.png"
+  },
+  {
+    "revision": "0642014b82e1333f7c82904f6415cd9e",
+    "url": "/static/media/spinner.0642014b.gif"
+  },
+  {
+    "revision": "fa6bf1763cfffbcb2689bb4b29198888",
+    "url": "/index.html"
+  }
+];
